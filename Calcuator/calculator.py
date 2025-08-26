3d15e873fafb8cb651cc98e1d2b18255289f00ab
Abdul basit <153916666+Abdulbasit3958@users.noreply.github.com>
2025-08-26 21:37:36 +0000

    first project

---
+++ Calcuator/calculator.py
@@ -0,0 +1,19 @@
+first= input("Enter your first number: ")
+operator= input("enter operator (+,-,*,/,&v): ")
+second= input("Enter your second nnumber: ")
+
+first= int(first)
+second= int(second)
+
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first-second)
+elif operator == "*":
+    print(first*second)
+elif operator == "/":
+    print(first/second)
+elif operator == "%":
+    print(first%second)
+else:
+    print("invalid operator")
